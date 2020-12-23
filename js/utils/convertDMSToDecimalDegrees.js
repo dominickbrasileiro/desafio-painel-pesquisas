c40a92418ac1a982c10d9102856c4f9c4fd16d35
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-12-22 21:50:18 -0300

    feat: round result in convertDMSToDecimalDegrees

--- js/utils/convertDMSToDecimalDegrees.js
+++ js/utils/convertDMSToDecimalDegrees.js
@@ -1,5 +1,7 @@
 // I've learned how to convert DMS to DD reading this article:
 // https://support.esri.com/en/technical-article/000007058
 function convertDMSToDecimalDegrees({ degree, minute, second }) {
-  return degree + (minute / 60) + (second / 3600);
+  const decimalDegrees = degree + (minute / 60) + (second / 3600);
+
+  return decimalDegrees.toPrecision(7);
 }
